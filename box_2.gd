d712552a1ad6f35ac48f436ee4e0099f1b3b62b2
Aura Amrah <auranawamadani@gmail.com>
2026-02-01 18:11:23 +0300

    box 2 something

--- box_2.gd
+++ box_2.gd
@@ -4,7 +4,7 @@ var player_in_range := false
 
 func _process(_delta: float) -> void:
 	if player_in_range and Input.is_action_just_pressed("wear"):
-		pass
+		pass #When clicked the character will be the one wearing covid mask
 
 func _on_area_2d_body_entered(body: Node) -> void:
 	if body.name == "Player":
